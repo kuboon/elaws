8ec63de557c20d25a78b3a584d89e161484e3d3b
ohkubo kohei <kuboon@trick-with.net>
2020-03-08 19:11:37 +0000

    404 page

--- api/page.ts
+++ api/page.ts
@@ -1,50 +1,51 @@
 import { NowRequest, NowResponse } from '@now/node'
 import pug from 'pug'
 import xmlParse from 'fast-xml-parser'
-import fs from 'fs'
-import { createTrue } from 'typescript'
+import jsonpath from 'jsonpath'
+import got from 'got'
 
-const blockElems = ["Part", "Chapter", "Article", "Paragraph", "Item", "Subitem1"]
-const page = pug.compileFile('data/page.pug')
+// const blockElems = ["Part", "Chapter", "Section", "Article", "Paragraph", "Item", "Subitem1"]
+const blockElems = ["Article", "Paragraph", "Item", "Subitem1"]
+const page = pug.compileFile('data/page.pug', {})
+
+class PathNotFound extends Error {}
 
 function xmlClose(xml: string) {
   return xml.replace(/<(.+)\/>/g, "<$1></$1>")
 };
-function selectByPath(json, pathname){
-  const path = pathname.split("/")[2]
+function selectByPath(json, path){
   if(!path || path == "") return
-  let cursor = json
+  let query = "$"
   path.split("-").forEach((v, i) => {
+    if(v=="0") return
     const name = blockElems[i]
-    const c2 = cursor[name]
-    if(!c2) throw new Error(JSON.stringify([cursor, name]))
-    cursor = c2.find(e => e["@_Num"] == v)
-    if(!cursor) throw new Error(JSON.stringify([c2, name, v]))
+    if(!name) throw "PathNotFound"
+    query += `..${name}[?(@.@_Num==${v})]`
   })
-  return cursor
+  const ret = jsonpath.query(json, query)
+  if(ret.length == 0) throw "PathNotFound"
+  return ret
 };
-function getSentence(str, json){
-  if(str.length > 100) return str;
-  let text;
-  Object.values(json).find(v => {
-    if(typeof v == 'object'){
-      text = v['#text']
-      if (text) return true;
-      str = getSentence(str, v)
-    }
-  })
-  return text ? str + text + " " : str;
+function getSentence(json){
+  return jsonpath.query(json, "$.._text").join(" ").slice(0,100)
 }
 export default async function(req: NowRequest, res: NowResponse) {
   const url = new URL(req.url, `http://${req.headers.host}`)
-  const xmlId = "417AC0000000086_20170401_428AC0000000062"
-  const xml = fs.readFileSync(`data/xml/${xmlId}.xml`).toString('utf8')
-  const json = xmlParse.parse(xml, {ignoreAttributes: false, arrayMode: "strict"}).Law[0].LawBody[0]
+  const [lawNum, path] = url.pathname.split("/").slice(1)
+  const source = "https://elaws.e-gov.go.jp/api/1/lawdata/" + lawNum
+  const xml = (await got(source)).body
+  const fullJson = xmlParse.parse(xml, {textNodeName : "_text", ignoreAttributes: false, arrayMode: "strict"})
+  const json = fullJson.DataRoot[0].ApplData[0].LawFullText[0].Law[0].LawBody[0]
   const title = json.LawTitle[0]
   const main = json.MainProvision[0]
-  console.log(url.pathname)
-  const target = selectByPath(main, url.pathname)
-  const sentence = target ? getSentence("", target) : ""
-  res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate')
-  res.send(page({xml: xmlClose(xml), title, sentence}))
+  try {
+    const target = selectByPath(main, path)
+    const sentence = target ? getSentence(target) : ""
+    res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate')
+    res.send(page({source, xml: xmlClose(xml), title, sentence}))
+  }catch(e){
+    if(e != "PathNotFound") throw e;
+    res.status(404)
+    res.send(pug.renderFile("data/nopath.pug", {lawNum, path}))
+  }
 }
--- src/page.js
+++ src/page.js
@@ -1,32 +1,27 @@
-const blockElems = ["Part", "Chapter", "Article", "Paragraph", "Item", "Subitem1"]
-function elemToLocation(el){
-  let loc = {}
-  blockElems.forEach(name => {
-    const container = el.closest(name)
-    if(!container)return;
-    loc[name] = container.attributes["Num"].value
-  })
-  return loc
-}
-function locationToPath(loc){
+// const blockElems = ["Part", "Chapter", "Section", "Article", "Paragraph", "Item", "Subitem1"]
+const blockElems = ["Article", "Paragraph", "Item", "Subitem1"]
+function elemToPath(el){
   let ret = []
   blockElems.forEach(name => {
-    if(loc[name]) ret.push(loc[name])
+    const container = el.closest(name)
+    ret.push(container ? container.attributes["Num"].value : "0")
   })
+  while(ret.slice(-1)=="0"){ ret.pop() }
   return ret.join("-")
 }
 function pathToElem(path){
   let selectors = []
   path.split("-").forEach((v, i) => {
+    if(v=="0") return
     const name = blockElems[i]
     selectors.push(`${name}[Num='${v}']`)
   })
   return document.querySelector(selectors.join(" "))
 }
 function onClick(ev){
-  const loc = elemToLocation(ev.target)
-  const path = locationToPath(loc)
-  history.pushState(null, path, "/%E4%BC%9A%E7%A4%BE%E6%B3%95/" + path)
+  const path = elemToPath(ev.target)
+  if(path == "") return
+  history.pushState(null, path, `/${document.location.pathname.split("/")[1]}/${path}`)
   select(pathToElem(path))
 }
 function select(el){
@@ -36,8 +31,8 @@ function select(el){
 }
 function selectByPath(){
   const {pathname} = document.location
-  const path = pathname.split("/").slice(-1)[0]
-  select(pathToElem(path))
+  const path = pathname.split("/")[2]
+  if(path)select(pathToElem(path))
 };
 window.addEventListener("load", selectByPath)
 window.addEventListener('popstate', selectByPath)
