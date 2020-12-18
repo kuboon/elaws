464218b1502bcf36285ac406b852ba1c0e796ef4
ohkubo kohei <kuboon@trick-with.net>
2020-12-18 16:23:55 +0900

    fix list

--- api/list.ts
+++ api/list.ts
@@ -2,8 +2,8 @@ import { NowRequest, NowResponse } from '@now/node'
 import xmlParse from 'fast-xml-parser'
 import got from 'got'
 
-const xmlP = got('https://elaws.e-gov.go.jp/api/1/lawlists/1')
 export default async function (req: NowRequest, res: NowResponse) {
+const xmlP = got('https://elaws.e-gov.go.jp/api/1/lawlists/1')
   const xml = await xmlP
   const json = xmlParse.parse(xml.body).DataRoot.ApplData.LawNameListInfo
   res.setHeader('Cache-Control', 's-maxage=3, stale-while-revalidate')
