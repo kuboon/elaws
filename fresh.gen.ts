3ebaa4f41e3793c38c16f64badacba8b97bd6944
Ohkubo KOHEI <kuboon@trick-with.net>
2022-03-27 08:31:52 +0000

    update sitemap

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,8 +6,8 @@ import * as $0 from "./routes/index.tsx";
 import * as $1 from "./routes/style.css.ts";
 import * as $2 from "./routes/[...id].tsx";
 import * as $3 from "./routes/_render.ts";
-import * as $4 from "./routes/sitemap.ts";
-import * as $5 from "./routes/page.js.ts";
+import * as $4 from "./routes/page.js.ts";
+import * as $5 from "./routes/sitemap.txt.ts";
 import * as $$0 from "./islands/LawList.tsx";
 
 const manifest = {
@@ -16,8 +16,8 @@ const manifest = {
     "./routes/style.css.ts": $1,
     "./routes/[...id].tsx": $2,
     "./routes/_render.ts": $3,
-    "./routes/sitemap.ts": $4,
-    "./routes/page.js.ts": $5,
+    "./routes/page.js.ts": $4,
+    "./routes/sitemap.txt.ts": $5,
   },
   islands: {
     "./islands/LawList.tsx": $$0,
