2c64d1b3ea27990a8d68ef5732f63c9dde14b703
Ohkubo KOHEI <kuboon@trick-with.net>
2022-05-11 10:46:02 +0000

    add no-check

--- server_deps.ts
+++ server_deps.ts
@@ -2,7 +2,7 @@ export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.
 export { renderToString } from "https://esm.sh/preact-render-to-string@5.1.20?deps=preact@10.6.6";
 export { default as jmespath } from "https://esm.sh/jmespath";
 
-import { XMLParser } from "https://esm.sh/fast-xml-parser@4";
+import { XMLParser } from "https://esm.sh/fast-xml-parser@4?no-check";
 export const xmlParser = new XMLParser({
   ignoreAttributes: false,
   isArray: () => true,
