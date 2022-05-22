dbba272de138e66bcfdb646f16c35fa0eedf79f8
Ohkubo KOHEI <kuboon@trick-with.net>
2022-05-22 23:53:02 +0000

    update my bundler

--- routes/page.js.ts
+++ routes/page.js.ts
@@ -1,11 +1,28 @@
 import { Handler } from "../server_deps.ts";
 import { esbuild, denoPlugin } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/src/server/deps.ts";
 
-const absWorkingDir = Deno.cwd();
+async function ensureEsbuildInialized() {
+  if (esbuildInitalized === false) {
+    if (Deno.run === undefined) {
+      esbuildInitalized = esbuild.initialize({
+        wasmURL: "https://unpkg.com/esbuild-wasm@0.14.39/esbuild.wasm",
+        worker: false,
+      });
+    } else {
+      esbuild.initialize({});
+    }
+    await esbuildInitalized;
+    esbuildInitalized = true;
+  } else if (esbuildInitalized instanceof Promise) {
+    await esbuildInitalized;
+  }
+}
 export const handler: Handler = async (_req, _ctx) => {
   const entryPoints: Record<string, string> = {
     "page": new URL("../lib/page.ts", import.meta.url).href,
   };
+  const absWorkingDir = Deno.cwd();
+  await ensureEsbuildInialized();
   const bundle = await esbuild.build({
     bundle: true,
     entryPoints,
