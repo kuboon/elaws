ae6ab183271d2a532026688c6ec19029b00e5c41
Ohkubo KOHEI <kuboon@trick-with.net>
2024-02-05 20:37:43 +0900

    use emit instead of esbuild

--- routes/page.js.ts
+++ routes/page.js.ts
@@ -1,44 +1,9 @@
 import { Handler } from "../server_deps.ts";
-import { denoPlugin } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/src/server/deps.ts";
-import * as esbuild from "https://unpkg.com/esbuild-wasm@0.14.38/esm/browser.js";
+import { bundle } from "https://deno.land/x/emit/mod.ts"
 
-let esbuildInitalized: boolean | Promise<void> = false;
-export async function ensureEsbuildInialized() {
-  if (esbuildInitalized === false) {
-    esbuildInitalized = esbuild.initialize({
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.14.38/esbuild.wasm",
-      worker: false,
-    });
-    await esbuildInitalized;
-    esbuildInitalized = true;
-  } else if (esbuildInitalized instanceof Promise) {
-    await esbuildInitalized;
-  }
-}
 export const handler: Handler = async (_req, _ctx) => {
-  const entryPoints: Record<string, string> = {
-    "page": new URL("../lib/page.ts", import.meta.url).href,
-  };
-  const absWorkingDir = Deno.cwd();
-  await ensureEsbuildInialized();
-  const bundle = await esbuild.build({
-    bundle: true,
-    entryPoints,
-    format: "esm",
-    metafile: true,
-    minify: false,
-    outdir: ".",
-    // This is requried to ensure the format of the outputFiles path is the same
-    // between windows and linux
-    absWorkingDir,
-    outfile: "",
-    platform: "neutral",
-    plugins: [denoPlugin()],
-    splitting: true,
-    target: ["chrome96", "firefox95", "safari14"],
-    treeShaking: true,
-    write: false,
-  });
+  const url = import.meta.resolve("../lib/page.ts");
+  const { code } = await bundle(url);
   const headers: Record<string, string> = { "Content-Type": "text/javascript" };
-  return new Response(bundle.outputFiles[0].text, { headers });
+  return new Response(code, { headers });
 };
