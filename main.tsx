182bbd6723293faaaf77e13fad8ca5c1e47ded5f
Ohkubo Kohei <kuboon@trick-with.net>
2024-03-29 16:51:31 +0000

    use emit instead of esbuild

--- main.tsx
+++ main.tsx
@@ -3,19 +3,13 @@
 
 import * as routes from "./routes/mod.ts"
 
+import { bundle } from "https://deno.land/x/emit@0.38.2/mod.ts"
 import { Hono } from "hono/mod.ts"
 import { jsx, serveStatic } from 'hono/middleware.ts'
-import * as esbuild from "https://deno.land/x/esbuild@v0.20.2/wasm.js";
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.10.3";
 
-async function bundle(entryPoint: string) {
-  const result = await esbuild.build({
-    plugins: [...denoPlugins()],
-    entryPoints: [import.meta.resolve(entryPoint)],
-    bundle: true,
-    format: "esm",
-  });
-  return result.outputFiles![0].text;
+async function bundleJs(entryPoint: string) {
+  const { code } = await bundle(import.meta.resolve(entryPoint));
+  return code
 }
 
 const app = new Hono()
@@ -27,14 +21,14 @@ app.get('/style.css', routes.styleCss)
 app.get('/BIZUDPGothic-Regular.ttf', serveStatic({path: "static/BIZUDPGothic-Regular.ttf"}))
 app.get('/list.mjs', async (c) => {
   const headers = { "Content-Type": "application/javascript" }
-  return new Response(await bundle("./islands/list.ts"), { headers })
+  return new Response(await bundleJs("./islands/list.ts"), { headers })
 })
 app.get('/favicon.:ext', serveStatic({path: "static/favicon.svg"}))
 app.get('/sitemap.txt', routes.sitemap)
 
 app.get('/page.js', async (c) => {
   const headers = { "Content-Type": "application/javascript" }
-  return new Response(await bundle("./islands/page.ts"), { headers })
+  return new Response(await bundleJs("./islands/page.ts"), { headers })
 })
 app.get('/:lawnum/:path?', (c) => {
   return routes.lawDetail(c, c.req.param('lawnum'), c.req.param('path'))
